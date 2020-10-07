c191d0a1f896438033557fb5d1bcfb923574159d
Neil Ipsen <ipsen.neil@gmail.com>
2020-10-07 17:26:01 -0400

    Added Pastebin.com token

--- dispaste.py
+++ dispaste.py
@@ -2,10 +2,17 @@ import os
 import discord
 from dotenv import load_dotenv
 
+load_dotenv()
+
 
 class DiscordInt:
     # Write __init__() here
 
-    load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
     client = discord.Client()
+
+
+class PastebinInt:
+    # Write __init__() here
+
+    TOKEN = os.getenv('PASTEBIN_TOKEN')
