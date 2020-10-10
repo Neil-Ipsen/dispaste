8fd049415fb1bd5ce31cbe1b310806985d10bb59
Neil-Ipsen <ipsen.neil@gmail.com>
2020-10-10 14:19:41 +0000

    Entered pastebin.com API options

--- dispaste.py
+++ dispaste.py
@@ -17,8 +17,18 @@ class PastebinInt:
     # Write __init__() here
 
     TOKEN = os.getenv('PASTEBIN_TOKEN')
-    url = ''
-    login = {}
-    paste = {}
+    url = 'https://pastebin.com/api/api_post.php'
+    text = ''
+    title = ''
+    #login = {}
+    paste = {
+        'api_option': 'paste',
+        'api_dev_key': TOKEN,
+        'api_paste_code': text,
+        'api_paste_name': title,
+        'api_paste_expire_date': 'see_https://pastebin.com/api',
+        'api_user_key': None,
+        'api_paste_format': 'see_https://pastebin.com/api'
+    }
     
     x = requests.post(url, data = paste)
